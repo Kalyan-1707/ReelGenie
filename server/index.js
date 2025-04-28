b626e5dc79039588acd58f03c0db774cb7276c08
Pavan Kalyan Konudula <konudulapavankalyan@gmail.com>
2025-04-28 19:34:08 -0400

    Configure CORS, specify Node.js version, and add vercel.json

--- server/index.js
+++ server/index.js
@@ -7,7 +7,19 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
